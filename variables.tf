5857bdecede6e7934966dfb95108d1b06fe5a69d
mehdi <lotfinejad@gmail.com>
2025-04-22 16:08:16 +0800

    Add initial Terraform project

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "aws_region" {
   description = "The AWS region to deploy resources in."
   type        = string
-  default     = "us-east-1"
+  default     = "us-east-1a"
 }
 
 variable "instance_type" {
