89d4d27c9a3891880931b7387a0756bf13050ba2
mehdi <lotfinejad@gmail.com>
2025-04-22 16:20:39 +0800

    Add initial Terraform project

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region  = "us-east-1"    
+  region  = var.aws_region
 
 }
 # Create a tiny EC2 instance
--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "aws_region" {
   description = "The AWS region to deploy resources in."
   type        = string
-  default     = "us-east-1a"
+  default     = "us-east-1"
 }
 
 variable "instance_type" {
