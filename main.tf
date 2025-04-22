ebaa02eb395f29f48f0290ec5630269117248890
mehdi <lotfinejad@gmail.com>
2025-04-22 16:23:00 +0800

    Add initial Terraform project

--- main.tf
+++ main.tf
@@ -1,17 +1,5 @@
-provider "aws" {
-  region  = var.aws_region
+# data "aws_caller_identity" "current" {}
 
-}
-# Create a tiny EC2 instance
-resource "aws_instance" "HelloEC2" {
-  ami           = "ami-04b4f1a9cf54c11d0" 
-  instance_type = var.instance_type
-
-  tags = {
-    Name = "EC2Instance"
-  }
-}
-# Output the public IP address of the instance
-output "instance_public_ip" {
-  value = aws_instance.HelloEC2.public_ip
+output "current_account_id" {
+  value = data.aws_caller_identity.current.account_id
 }
